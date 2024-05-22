55a7dc02f5669469bab22cc309d7a9966b4557ff
eooce <142894633+eooce@users.noreply.github.com>
2024-05-22 15:53:27 +0800

    Update reality.sh

--- reality.sh
+++ reality.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 export PORT=${PORT:-'8880'}
-export UUID=$(cat /proc/sys/kernel/random/uuid)
+export UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
 
 # 检查是否为root下运行
-[[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && sleep 2 && exit 1
+[[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && sleep 1 && exit 1
+
+# 安装依赖
+install() {
+    packages="gawk curl openssl qrencode"
+    install=""
+
+    for pkg in $packages; do
+        if ! command -v $pkg &>/dev/null; then
+            install="$install $pkg"
+        fi
+    done
+
+    if [ -z "$install" ]; then
+        echo -e "\e[1;32mAll packages are already installed\e[0m"
+        return
+    fi
+
+    if command -v apt &>/dev/null; then
+        pm="apt-get install -y -q"
+    elif command -v dnf &>/dev/null; then
+        pm="dnf install -y"
+    elif command -v yum &>/dev/null; then
+        pm="yum install -y"
+    elif command -v apk &>/dev/null; then
+        pm="apk add"
+    else
+        echo -e "\e[1;33m暂不支持的系统!\e[0m"
+        exit 1
+    fi
+    $pm $install
+    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+}
 
 # 获取IP地址
 getIP() {
     local serverIP
     serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
     if [[ -z "${serverIP}" ]]; then
-        serverIP=$(curl -s https://ipv6.icanhazip.com)
+        serverIP=$(curl -s --max-time 1 ipv6.ip.sb)
     fi
     echo "${serverIP}"
 }
 
-# 安装xray
-install_xray() {
-    if command -v apt &>/dev/null; then
-        apt-get update -y
-        apt-get install -y gawk curl openssl qrencode
-    elif command -v dnf &>/dev/null; then
-        dnf update -y
-        dnf install -y epel-release gawk curl openssl qrencode
-    elif command -v yum &>/dev/null; then
-        yum update -y
-        yum install -y epel-release gawk curl openssl qrencode
-    else
-        echo -e "${red}暂不支持你的系统!${re}"
-        return 1
-    fi
-    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
-}
-
-
+# 配置Xray
 reconfig() {
     reX25519Key=$(/usr/local/bin/xray x25519)
     rePrivateKey=$(echo "${reX25519Key}" | head -1 | awk '{print $3}')
     rePublicKey=$(echo "${reX25519Key}" | tail -n 1 | awk '{print $3}')
     shortId=$(openssl rand -hex 8)
 
-    # 重新配置Xray
     cat >/usr/local/etc/xray/config.json <<EOF
 {
     "inbounds": [
@@ -111,5 +124,5 @@ EOF
 
 }
 
-install_xray
+install
 reconfig
