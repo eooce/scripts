73170ef53798eed08d9bb7ca489f446693c7280c
eooce <142894633+eooce@users.noreply.github.com>
2024-12-22 13:34:06 +0800

    Update 00-vless.sh

--- containers-shell/00-vless.sh
+++ containers-shell/00-vless.sh
@@ -84,42 +84,16 @@ generate_config() {
       "https+local://8.8.8.8/dns-query"
     ]
   },
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    },
-    {
-      "tag": "WARP",
-      "protocol": "wireguard",
-      "settings": {
-        "secretKey": "cKE7LmCF61IhqqABGhvJ44jWXp8fKymcMAEVAzbDF2k=",
-        "address": [
-          "172.16.0.2/32",
-          "fd01:5ca1:ab1e:823e:e094:eb1c:ff87:1fab/128"
-        ],
-        "peers": [
-          {
-            "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-            "endpoint": "162.159.193.10:2408"
-          }
-        ]
-      }
-    }
-  ],
-  "routing": {
-    "domainStrategy": "AsIs",
-    "rules": [
-      {
-        "type": "field",
-        "domain": [
-          "domain:openai.com",
-          "domain:chatgpt.com",
-          "domain:chat.openai.com"
-        ],
-        "outboundTag": "WARP"
-      }
+   "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "block"
+        }
     ]
-  }
 }
 EOF
 }
